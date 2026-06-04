28860f6a563a2be59c0087f4a7a4495b98b542f0
KASHIF290 <kashifjalal290@gmail.com>
2026-06-04 07:28:14 +0530

    Added Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
